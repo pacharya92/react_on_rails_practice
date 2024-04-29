ef09b3f576146254279c39e00b0982bd16f7035d
Prakash Acharya <prakashacharya1992@gmail.com>
2024-04-28 23:58:51 -0400

    Setup api v1 routes and rubcop

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # API should be in /api/v1
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+    end
+  end
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
